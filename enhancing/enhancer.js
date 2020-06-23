74e9eb9a482ece1870d92ea77a47945cd6b59d19
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-06-23 08:14:57 -0500

    Finished functions to test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,10 +6,23 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  succeededItem = item;
+  if(succeededItem.enhancement > 19)
+  return { ...succeededItem };
+  else return {...succeededItem, enchancement: succeededItem.enhancement + 1 }
 }
 
 function fail(item) {
+  failedItem = item;
+  if(failedItem.enhancement < 15) {
+    return {...failedItem, durability: failedItem.durability - 5 }
+  }
+  else if(failedItem.enhancement === 15 || failedItem.enhancement === 16) {
+    return {...failedItem, durability: failedItem.durability - 10 }
+  }
+  else if(failedItem.enhancement > 16) {
+    return {...failedItem, enhancement: failedItem.enhancement - 1 }
+  }
   return { ...item };
 }
 
