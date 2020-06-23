0278e0b5bd8f652d1f7f6d4f6cac53962035b449
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-06-23 09:54:25 -0500

    Removed bad describe import from test file

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,15 +1,14 @@
 const {repair, succeed, fail, get} = require('./enhancer.js');
-const { describe } = require('yargs');
 
-describe("enhancement functions", () => {
+describe('enhancement functions', () => {
 
-    describe("succeed()", () => {
-        it("should increase enhancement by 1 unless enhancement is already 20", () => {
+    describe('succeed()', () => {
+        it('should increase enhancement by 1 unless enhancement is already 20', () => {
             const item = {name: "Mace", durability: 75, enhancement: 15};
             const masterItem = {name: "Mace", durability: 75, enhancement: 20};
             
             expect(succeed(item)).toEqual({name: "Mace", durability: 75, enhancement: 16 });
             expect(succeed(masterItem)).toEqual(masterItem);
-        })
+        });
     });
-})
+});
